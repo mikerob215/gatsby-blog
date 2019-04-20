8a151fc5fa6b09c25dc4e5efca3b00178524746b
Mike Robinson <mikerobinson1@gmail.com>
2019-04-19 20:19:18 -0400

    Fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Mike Robinsons awful thoughts</h1>
+    <h1>Mike Robinson's awful thoughts</h1>
     <p>In the future you will be full o the things I annoy friends and coworkers with</p>
   </Layout>
 )
